26542530849596721e3e4392330c88317041ec22
Gilvan <gilvanritter@gmail.com>
2019-02-04 12:10:46 -0200

    created Library entity class to represent a list of books

---
+++ src/main/java/entity/Library.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.util.List;
+
+public class Library {
+
+    private List<Book> books;
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
