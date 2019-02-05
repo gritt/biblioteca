2cb0da2dfefdc9f889d98bb228f4ae49343997f6
Gilvan <gilvanritter@gmail.com>
2019-02-05 18:42:46 -0200

    created mock class for empty library

---
+++ src/test/java/entity/MockEmptyLibrary.java
@@ -0,0 +1,18 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockEmptyLibrary implements LibraryInterface {
+
+    @Override
+    public void setBooks(List<Book> books) {
+        // do nothing
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        List<Book> books = new ArrayList<>();
+        return books;
+    }
+}
