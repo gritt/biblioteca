b8179a76e6706b012810f0906b9025e4aaebe7c1
Gilvan <gilvanritter@gmail.com>
2019-02-05 18:11:45 -0200

    created mock list of Books in MockLibrary

--- src/test/java/entity/MockLibrary.java
+++ src/test/java/entity/MockLibrary.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MockLibrary implements LibraryInterface {
@@ -11,7 +13,33 @@ public class MockLibrary implements LibraryInterface {
 
     @Override
     public List<Book> getBooks() {
-        // return a list of fake books
-        return  null;
+
+        Book designPatterns = new Book()
+                .setId(1)
+                .setName("Design Patterns, Elements of Reusable Object-Oriented-Software")
+                .setAuthor("Erich Gama")
+                .setCopies(10)
+                .setLaunchDate(new Date("now"));
+
+        Book cleanCode = new Book()
+                .setId(2)
+                .setName("Clean Code")
+                .setAuthor("Robert C Martin")
+                .setCopies(5)
+                .setLaunchDate(new Date("now"));
+
+        Book theCleanCoder = new Book()
+                .setId(3)
+                .setName("The Clean Coder")
+                .setAuthor("Robert C Martin")
+                .setCopies(3)
+                .setLaunchDate(new Date("now"));
+
+        List<Book> books = new ArrayList<>();
+        books.add(designPatterns);
+        books.add(cleanCode);
+        books.add(theCleanCoder);
+
+        return books;
     }
 }
