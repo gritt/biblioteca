3e6c8f74fb2f3ef8ce9ccc869b02080f3112b3c3
Gilvan <gilvanritter@gmail.com>
2019-02-06 18:53:37 -0200

    asserted that and exceptions ins thrown when a invalid option is given

--- src/test/java/service/MenuTest.java
+++ src/test/java/service/MenuTest.java
@@ -1,13 +1,17 @@
 package service;
 
 import io.FakePrintStream;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
+
+    @Rule
+    public ExpectedException exceptionGrabber = ExpectedException.none();
 
     @Test
     void shouldShowMenu() {
@@ -21,10 +25,25 @@ class MenuTest {
     }
 
     @Test
-    void shouldReturnActionWhenGivingThenId() {
+    void shouldReturnActionWhenGivingThenId() throws IndexOutOfBoundsException {
+
+        FakePrintStream printStream = new FakePrintStream();
+        Menu menu = new Menu(printStream);
+
+        String method = menu.getAction(0);
+        assertThat(method, is("listBooks"));
     }
 
     @Test
-    void should() {
+    void shouldThrowExceptionWhenInvalidOptionGiven() throws IndexOutOfBoundsException {
+
+        FakePrintStream printStream = new FakePrintStream();
+        Menu menu = new Menu(printStream);
+
+        try {
+            menu.getAction(4);
+        } catch (IndexOutOfBoundsException e) {
+            assertThat(e.getMessage(), is("Invalid option given, please try again \n"));
+        }
     }
 }
