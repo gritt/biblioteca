362e95d01be83b7eef4debca7f2a39aa08b34a70
Gilvan <gilvanritter@gmail.com>
2019-02-06 12:09:05 -0200

    added name to Menu, toString() prints the menu line

--- src/main/java/service/MenuOption.java
+++ src/main/java/service/MenuOption.java
@@ -4,14 +4,17 @@ public class MenuOption {
 
     private int id;
     private String method;
+    private String name;
 
-    public MenuOption(int id, String method) {
+    public MenuOption(int id, String method, String name) {
         this.id = id;
         this.method = method;
+        this.name = name;
     }
 
-    public int getId() {
-        return id;
+    @Override
+    public String toString() {
+        return "[" + id + "] " + name + "\n";
     }
 
     public String getMethod() {
