db11ce57c893667ea15b376cd084f70e87e1c3fe
Gilvan <gilvanritter@gmail.com>
2019-02-07 14:10:41 -0200

    fixed indexes for MockLibrary

--- src/test/java/entity/MockLibrary.java
+++ src/test/java/entity/MockLibrary.java
@@ -14,19 +14,19 @@ public class MockLibrary implements LibraryInterface {
     public List<Book> getBooks() {
 
         Book designPatterns = new Book()
-                .setId(1)
+                .setId(0)
                 .setName("Design Patterns, Elements of Reusable Object-Oriented-Software")
                 .setAuthor("Erich Gama")
                 .setYear(1994);
 
         Book cleanCode = new Book()
-                .setId(2)
+                .setId(1)
                 .setName("Clean Code")
                 .setAuthor("Robert C Martin")
                 .setYear(2008);
 
         Book theCleanCoder = new Book()
-                .setId(3)
+                .setId(2)
                 .setName("The Clean Coder")
                 .setAuthor("Robert C Martin")
                 .setYear(2011);
