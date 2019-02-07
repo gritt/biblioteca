f899a00f704abba9a77f4625edafea0616627894
Gilvan <gilvanritter@gmail.com>
2019-02-07 17:18:18 -0200

    added test to assert book checkout feature, improved mocking to use the real class

--- src/test/java/service/LibraryTest.java
+++ src/test/java/service/LibraryTest.java
@@ -1,27 +1,64 @@
 package service;
 
+import entity.Book;
+import entity.LibraryInterface;
 import entity.MockEmptyLibrary;
-import entity.MockLibrary;
 import io.FakePrintStream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 class LibraryTest {
 
+    private LibraryInterface mockLibrary;
+
+    @BeforeEach
+    public void setupMockLibrary() {
+        List<Book> fakeBooks = new ArrayList<>();
+
+        Book designPatterns = new Book()
+                .setId(0)
+                .setName("Book A")
+                .setAuthor("Erich Gama")
+                .setYear(1994);
+
+        Book cleanCode = new Book()
+                .setId(1)
+                .setName("Book B")
+                .setAuthor("Robert C Martin")
+                .setYear(2008);
+
+        fakeBooks.add(designPatterns);
+        fakeBooks.add(cleanCode);
+
+        mockLibrary = new entity.Library(fakeBooks);
+
+
+        Book theCleanCoder = new Book()
+                .setId(2)
+                .setName("Book C")
+                .setAuthor("Robert C Martin")
+                .setYear(2011);
+
+        mockLibrary.addReservedBook(theCleanCoder);
+    }
+
     @Test
     public void shouldListBooks() {
 
         FakePrintStream printStream = new FakePrintStream();
+        Library library = new Library(mockLibrary, printStream, System.in);
 
-        Library library = new Library(new MockLibrary(), printStream);
         library.listBooks();
 
         assertThat(printStream.printedString(), is(
-                "[1] Design Patterns, Elements of Reusable Object-Oriented-Software - Erich Gama. (1994)\n" +
-                        "[2] Clean Code - Robert C Martin. (2008)\n" +
-                        "[3] The Clean Coder - Robert C Martin. (2011)\n"
+                "[0] Book A - Erich Gama. (1994)\n" +
+                        "[1] Book B - Robert C Martin. (2008)\n"
         ));
     }
 
@@ -29,10 +66,49 @@ class LibraryTest {
     public void shouldListEmptyWhenThereAreNoBooks() {
 
         FakePrintStream printStream = new FakePrintStream();
+        Library library = new Library(new MockEmptyLibrary(), printStream, System.in);
 
-        Library library = new Library(new MockEmptyLibrary(), printStream);
         library.listBooks();
 
         assertThat(printStream.printedString(), is(""));
+    }
+
+    @Test
+    public void shouldListReservedBooks() {
+        FakePrintStream printStream = new FakePrintStream();
+        Library library = new Library(mockLibrary, printStream, System.in);
+
+        library.listReservedBooks();
+
+        assertThat(printStream.printedString(), is("[2] Book C - Robert C Martin. (2011)\n"));
+    }
+
+    @Test
+    public void shouldCheckoutBook() {
+        FakePrintStream printStream = new FakePrintStream();
+        Library library = new Library(mockLibrary, printStream, System.in);
+
+        library.setBookId(0);
+        library.checkoutBook();
+        library.listReservedBooks();
+
+        assertThat(printStream.printedString(), is(
+                "[2] Book C - Robert C Martin. (2011)\n" +
+                        "[0] Book A - Erich Gama. (1994)\n"
+
+        ));
+    }
+
+    @Test
+    public void shouldRemoveBookFromListWhenCheckoutIsSuccessful() {
+
+        FakePrintStream printStream = new FakePrintStream();
+        Library library = new Library(mockLibrary, printStream, System.in);
+
+        library.setBookId(0);
+        library.checkoutBook();
+        library.listBooks();
+
+        assertThat(printStream.printedString(), is("[2] Book B - Robert C Martin. (2011)\n"));
     }
 }
