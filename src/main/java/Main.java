c2088140039061ac0bff3b9de558b71671597c59
Gilvan <gilvanritter@gmail.com>
2019-02-07 00:08:25 -0200

    Main calls service.library to execute the action which was read and parsed from user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,39 @@
-import controller.Biblioteca;
-import entity.Library;
-import service.GreetingPrinter;
+import io.Reader;
+import service.Library;
+import service.Welcome;
+import service.Menu;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        GreetingPrinter greetingPrinter = new GreetingPrinter(System.out);
-        greetingPrinter.printWelcome();
+        Welcome welcome = new Welcome(System.out);
+        welcome.show();
 
-        Biblioteca biblioteca = new Biblioteca(new Library(), System.out);
-        biblioteca.listBooks();
+        Library library = new Library(
+                new entity.Library(),
+                System.out
+        );
+
+
+        Menu menu = new Menu(System.out);
+        Reader reader = new Reader(System.in);
+
+        int status = Menu.CONTINUE;
+        do {
+            try {
+                menu.show();
+
+                status = library.execute(
+                        menu.getAction(reader.readNumber())
+                );
+
+            } catch (Exception e) {
+                continue;
+            }
+
+        } while (status == Menu.CONTINUE);
+
+        System.exit(0);
     }
 }
