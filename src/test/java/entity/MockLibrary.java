a35f05f050e8c2f27fb648b67a0dcc968b00a646
Gilvan <gilvanritter@gmail.com>
2019-02-07 14:03:32 -0200

    removed copies attribute from books

--- src/test/java/entity/MockLibrary.java
+++ src/test/java/entity/MockLibrary.java
@@ -17,21 +17,18 @@ public class MockLibrary implements LibraryInterface {
                 .setId(1)
                 .setName("Design Patterns, Elements of Reusable Object-Oriented-Software")
                 .setAuthor("Erich Gama")
-                .setCopies(10)
                 .setYear(1994);
 
         Book cleanCode = new Book()
                 .setId(2)
                 .setName("Clean Code")
                 .setAuthor("Robert C Martin")
-                .setCopies(5)
                 .setYear(2008);
 
         Book theCleanCoder = new Book()
                 .setId(3)
                 .setName("The Clean Coder")
                 .setAuthor("Robert C Martin")
-                .setCopies(3)
                 .setYear(2011);
 
         List<Book> books = new ArrayList<>();
