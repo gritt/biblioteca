c3bb4f8a0ed2491cd35b912a579ed2d57081ac37
Gilvan <gilvanritter@gmail.com>
2019-02-07 17:22:12 -0200

    added methods to checkout book and to list reserved books

--- src/main/java/service/Library.java
+++ src/main/java/service/Library.java
@@ -1,20 +1,28 @@
 package service;
 
+import entity.Book;
 import entity.LibraryInterface;
+import io.Reader;
 import io.Writer;
 
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Library {
 
-    private Writer writer;
+    private Writer ioWriter;
+    private Reader ioReader;
     private LibraryInterface library;
+
+    private int bookId = -1;
     private int status;
 
-    public Library(LibraryInterface library, PrintStream printStream) {
-        this.writer = new Writer(printStream);
+
+    public Library(LibraryInterface library, PrintStream printStream, InputStream inputStream) {
+        this.ioWriter = new Writer(printStream);
+        this.ioReader = new Reader(inputStream);
         this.library = library;
     }
 
@@ -27,6 +35,7 @@ public class Library {
         return status;
     }
 
+
     public void listBooks() {
 
         StringBuilder listOfBooks = new StringBuilder();
@@ -35,10 +44,63 @@ public class Library {
             listOfBooks.append(book.toString() + "\n");
         });
 
-        writer.print(listOfBooks.toString());
+        ioWriter.print(listOfBooks.toString());
 
         status = Menu.CONTINUE;
     }
+
+    public void listReservedBooks() {
+
+        StringBuilder listOfReservedBooks = new StringBuilder();
+
+        library.getReservedBooks().forEach(book -> {
+            listOfReservedBooks.append(book.toString() + "\n");
+        });
+
+        ioWriter.print(listOfReservedBooks.toString());
+
+        status = Menu.CONTINUE;
+    }
+
+
+    public void checkoutBook() {
+        try {
+
+            while (!hasInformedBookId()) {
+                bookId = readBookId();
+            }
+
+            Book book = library.getBooks().get(bookId);
+
+            library.removeBook(book);
+            library.addReservedBook(book);
+
+        } catch (Exception ex) {
+            //TODO @gritt, show error message if failed to checkout book
+            return;
+        }
+
+        status = Menu.CONTINUE;
+    }
+
+    private boolean hasInformedBookId() {
+        return bookId != -1;
+    }
+
+    private int readBookId () {
+        listBooks();
+
+        ioWriter.print("\nPlease choose a book from the list [number]: \n");
+
+        return ioReader.readNumber();
+    }
+
+    public Library setBookId(int bookId) {
+        this.bookId = bookId;
+        return this;
+    }
+
+
 
     public void close() {
         status = Menu.QUIT;
