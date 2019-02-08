4308acee92dbb7189dcdeefdf5fb0a86537c00f3
Gilvan <gilvanritter@gmail.com>
2019-02-08 02:10:18 -0200

    added TODO, tmp moved getBooks with made up list of books to LibraryService

--- src/main/java/service/LibraryService.java
+++ src/main/java/service/LibraryService.java
@@ -3,6 +3,9 @@ package service;
 import entity.Book;
 import entity.Library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryService {
 
     private Library library;
@@ -66,5 +69,51 @@ public class LibraryService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+
+    //TODO @gritt, remove getBooks from this class
+    public static List<Book> getBooks() {
+
+        List<Book> books = new ArrayList<>();
+
+        Book designPatterns = new Book()
+                .setName("Design Patterns, Elements of Reusable Object-Oriented-Software")
+                .setAuthor("Erich Gama")
+                .setYear(1994);
+
+        Book cleanCode = new Book()
+                .setName("Clean Code")
+                .setAuthor("Robert C Martin")
+                .setYear(2008);
+
+        Book theCleanCoder = new Book()
+                .setName("The Clean Coder")
+                .setAuthor("Robert C Martin")
+                .setYear(2011);
+
+        Book commandLineKungFu = new Book()
+                .setName("Command Line Kung Fu")
+                .setAuthor("Jason Cannon")
+                .setYear(2014);
+
+        Book dataScienceFromScratch = new Book()
+                .setName("Data Science From Scratch")
+                .setAuthor("Joel Grus")
+                .setYear(2015);
+
+        Book pythonTheBible = new Book()
+                .setName("Python, The Bible")
+                .setAuthor("Maurice J. Thompson")
+                .setYear(2018);
+
+        books.add(designPatterns);
+        books.add(cleanCode);
+        books.add(theCleanCoder);
+        books.add(commandLineKungFu);
+        books.add(dataScienceFromScratch);
+        books.add(pythonTheBible);
+
+        return books;
     }
 }
