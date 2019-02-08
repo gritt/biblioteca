ef5eb70ba5d793a0f0bb3824ce78b6ae8f061022
Gilvan <gilvanritter@gmail.com>
2019-02-08 01:32:04 -0200

    created MenuService which child classes can extends to access options

---
+++ src/main/java/service/menu/MenuService.java
@@ -0,0 +1,26 @@
+package service.menu;
+
+import io.Reader;
+import io.Writer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuService {
+
+    private Writer writer;
+    private Reader reader;
+
+    private List<Option> options = new ArrayList<>();
+
+    public MenuService(Writer writer, Reader reader) {
+        this.writer = writer;
+        this.reader = reader;
+
+        options.add(new Option(1, "List books"));
+        options.add(new Option(2, "List reservations"));
+        options.add(new Option(3, "Checkout a book"));
+        options.add(new Option(4, "Return a book"));
+        options.add(new Option(0, "Close"));
+    }
+}
