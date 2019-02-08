0340f970d07e0fe6a5177cda73fab87a8c41f2ea
Gilvan <gilvanritter@gmail.com>
2019-02-08 02:08:59 -0200

    handle switch case through the LibraryService options registered on menu

--- src/main/java/service/menu/MenuRouterService.java
+++ src/main/java/service/menu/MenuRouterService.java
@@ -1,26 +1,79 @@
 package service.menu;
 
-
+import entity.Library;
 import io.Reader;
 import io.Writer;
+import service.LibraryService;
 
-public class MenuRouterService extends MenuService{
+public class MenuRouterService extends MenuService {
+
+    private LibraryService libraryService;
 
     public MenuRouterService(Writer writer, Reader reader) {
+
         super(writer, reader);
+
+        /*
+         * init library service with some books
+         */
+
+        // TODO @gritt, refactor where does books source, best fit might not be LibraryService.getBooks()
+        libraryService = new LibraryService();
+
+        libraryService.setLibrary(
+                new Library(LibraryService.getBooks())
+        );
     }
 
     public boolean handle() {
 
         int option = getMenuOptionFromStdin();
 
-//        menuOptions.add(new Option(1, "List books"));
-//        menuOptions.add(new Option(2, "List reservations"));
-//        menuOptions.add(new Option(3, "Checkout a book"));
-//        menuOptions.add(new Option(4, "Return a book"));
-//        menuOptions.add(new Option(0, "Close"));
+        switch (option) {
 
-        return false;
+            case 1: {
+                String response = libraryService.listBooks();
+                writer.print(response);
+                return true;
+            }
+            case 2: {
+                String response = libraryService.listReservations();
+                writer.print(response);
+                return true;
+            }
+            case 3: {
+                int targetBookId = getNumberOptionFromStdin();
+
+                String response = "Sorry, that book is unavailable.\n";
+                if (libraryService.checkoutBook(targetBookId)) {
+                    response = "Thank yoy! Enjoy the book.\n";
+                }
+
+                writer.print(response);
+                return true;
+            }
+            case 4: {
+                int targetBookId = getNumberOptionFromStdin();
+
+                String response = "That's not a valid book to return.\n";
+                if (libraryService.returnBook(targetBookId)) {
+                    response = "Thank you for returning the book.\n";
+                }
+
+                writer.print(response);
+                break;
+            }
+            case 0: {
+                writer.print("Bye Bye! \n");
+                appShouldKeepRunning = false;
+                break;
+            }
+            default: {
+                return true;
+            }
+        }
+
+        return appShouldKeepRunning;
     }
 
     private int getMenuOptionFromStdin() {
@@ -30,7 +83,7 @@ public class MenuRouterService extends MenuService{
 
             for (Option menuOption : options) {
                 if (menuOption.getId() == targetOption) {
-                    writer.print(menuOption.toString());
+                    writer.print("........................"+menuOption.getName()+"........................");
                     return targetOption;
                 }
             }
@@ -40,6 +93,17 @@ public class MenuRouterService extends MenuService{
         } catch (Exception e) {
             writer.print("Invalid option, please try again: ");
             return getMenuOptionFromStdin();
+        }
+    }
+
+    private int getNumberOptionFromStdin() {
+        try {
+
+            return reader.readNumber();
+
+        } catch (Exception e) {
+            writer.print("Invalid option, please try again: ");
+            return getNumberOptionFromStdin();
         }
     }
 }
