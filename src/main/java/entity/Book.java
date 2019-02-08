b421ca867b84e890d740d69e5091ead48b845bac
Gilvan <gilvanritter@gmail.com>
2019-02-07 22:13:57 -0200

    simplified Book class

--- src/main/java/entity/Book.java
+++ src/main/java/entity/Book.java
@@ -2,27 +2,14 @@ package entity;
 
 public class Book {
 
-    private Integer id;
     private String name;
     private String author;
     private int year;
 
+
     @Override
     public String toString() {
-        return "[" + id + "] " + name + " - " + author + ". (" + year + ")";
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Book setId(Integer id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getName() {
-        return name;
+        return name + " - " + author + ". (" + year + ")";
     }
 
     public Book setName(String name) {
@@ -30,17 +17,9 @@ public class Book {
         return this;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
     public Book setAuthor(String author) {
         this.author = author;
         return this;
-    }
-
-    public int getYear() {
-        return year;
     }
 
     public Book setYear(int year) {
