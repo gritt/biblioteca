f8fc024675dc9541b9ee51565f5a4e3a013c4fc0
Gilvan <gilvanritter@gmail.com>
2019-02-08 13:59:34 -0200

    updated code to handle a hashmap instead of a list<book>

--- src/main/java/service/LibraryService.java
+++ src/main/java/service/LibraryService.java
@@ -3,117 +3,71 @@ package service;
 import entity.Book;
 import entity.Library;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LibraryService {
 
     private Library library;
-
-
-    public Library getLibrary() {
-        return library;
-    }
 
     public LibraryService setLibrary(Library library) {
         this.library = library;
         return this;
     }
 
-
     public String listBooks() {
 
         StringBuilder listOfBooks = new StringBuilder();
 
-        for (int i = 0; i < library.getBooks().size(); i++) {
-            listOfBooks.append("[" + i + "]. " + library.getBooks().get(i).toString() + "\n");
-        }
+        library.getBooks().forEach((integer, book) -> {
+            if (book.isAvailable()) {
+                listOfBooks.append(book.toString() + "\n");
+            }
+        });
+
 
         return listOfBooks.toString();
     }
 
     public String listReservations() {
 
-        StringBuilder listOfReservedBooks = new StringBuilder();
+        StringBuilder listOfReservations = new StringBuilder();
 
-        for (int i = 0; i < library.getReservations().size(); i++) {
-            listOfReservedBooks.append("[" + i + "]. " + library.getReservations().get(i).toString() + "\n");
-        }
+        library.getBooks().forEach((integer, book) -> {
+            if (!book.isAvailable()) {
+                listOfReservations.append(book.toString() + "\n");
+            }
+        });
 
-        return listOfReservedBooks.toString();
+        return listOfReservations.toString();
     }
 
     public boolean checkoutBook(int bookId) {
-        try {
 
-            Book targetBook = library.getBooks().get(bookId);
-            library.removeBook(targetBook);
-            library.addReservation(targetBook);
-
-            return true;
-
-        } catch (Exception e) {
+        if (!library.getBooks().containsKey(bookId)) {
             return false;
         }
+
+        Book targetBook = library.getBooks().get(bookId);
+
+        if (!targetBook.isAvailable()) {
+            return false;
+        }
+
+        targetBook.setAvailable(false);
+        return true;
     }
 
     public boolean returnBook(int bookId) {
-        try {
 
-            Book targetBook = library.getReservations().get(bookId);
-            library.removeReservation(targetBook);
-            library.addBook(targetBook);
-
-            return true;
-
-        } catch (Exception e) {
+        if (!library.getBooks().containsKey(bookId)) {
             return false;
         }
-    }
 
+        Book targetBook = library.getBooks().get(bookId);
 
-    //TODO @gritt, remove getBooks from this class
-    public static List<Book> getBooks() {
+        if (targetBook.isAvailable()) {
+            return false;
+        }
 
-        List<Book> books = new ArrayList<>();
-
-        Book designPatterns = new Book()
-                .setName("Design Patterns, Elements of Reusable Object-Oriented-Software")
-                .setAuthor("Erich Gama")
-                .setYear(1994);
-
-        Book cleanCode = new Book()
-                .setName("Clean Code")
-                .setAuthor("Robert C Martin")
-                .setYear(2008);
-
-        Book theCleanCoder = new Book()
-                .setName("The Clean Coder")
-                .setAuthor("Robert C Martin")
-                .setYear(2011);
-
-        Book commandLineKungFu = new Book()
-                .setName("Command Line Kung Fu")
-                .setAuthor("Jason Cannon")
-                .setYear(2014);
-
-        Book dataScienceFromScratch = new Book()
-                .setName("Data Science From Scratch")
-                .setAuthor("Joel Grus")
-                .setYear(2015);
-
-        Book pythonTheBible = new Book()
-                .setName("Python, The Bible")
-                .setAuthor("Maurice J. Thompson")
-                .setYear(2018);
-
-        books.add(designPatterns);
-        books.add(cleanCode);
-        books.add(theCleanCoder);
-        books.add(commandLineKungFu);
-        books.add(dataScienceFromScratch);
-        books.add(pythonTheBible);
-
-        return books;
+        targetBook.setAvailable(true);
+        return true;
     }
 }
